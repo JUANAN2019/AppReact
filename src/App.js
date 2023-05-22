9833037502c6fc7205b6ad553603a656b81e472f
marcela romero <natisevo25@gmail.com>
2023-05-22 13:47:57 +0200

    prueba

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Footer from './Components/Footer';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://randomuser.me/api/?results=12');
+        const data = await response.json();
+        setUsers(data.results);
+      } catch (error) {
+        console.log('Error al obtener los datos:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleRefresh = () => {
+    window.location.reload();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Random Users</h1>
+      <div className="button-container">
+        <button className="refresh-button" onClick={handleRefresh}>
+          Actualizar
+        </button>
+      </div>
+      <div className="grid-container">
+        {users.map(user => (
+          <div className="grid-item" key={user.login.uuid}>
+            <img src={user.picture.thumbnail} alt={user.name.first} />
+            <span>{user.name.first} {user.name.last}</span>
+          </div>
+        ))}
+      </div>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
