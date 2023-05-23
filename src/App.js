3393a1e6c9a37dafa0cb6811f26430e369d78d3e
Greiskel <greiskel.sanchez@gmail.com>
2023-05-23 09:03:55 +0200

    actualización de main

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import Mainpage from './Components/Main';
 import Characters from './Components/Characters';
 import Navbar from './Components/Navbar';
 
+
 function App() {
   return (
     <>
-       <Navbar />
+      <Navbar />
 
       <bubbles />
-       
+
       <BrowserRouter >
         <Routes >
-        <Route  path='/' element={<Mainpage />}></Route>
-        <Route path='/characters/:id' element={<Characters />}></Route> 
+          <Route path='/' element={<Mainpage />}></Route>
+          <Route path='/characters/:id' element={<Characters />}></Route>
         </Routes>
-      </BrowserRouter>                          
-
-   </>
+      </BrowserRouter>
+    </>
   );
 }
 
